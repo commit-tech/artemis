8c9ee301e65a519a0256ff1f139b6228d4fc825c
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-02-23 19:58:14 +0800

    Recess week wishes

--- Utils/Constants_template.php
+++ Utils/Constants_template.php
@@ -14,5 +14,5 @@ define("NUM_EMAIL_THREAD",12);
 define("DOMAIN","api.nussucommit.com");
 
 // Message to be displayed at the end of every e-mail sent automatically through this website
-define("FOOTER_MESSAGE","\nNUSSU commIT wishes you the best in your exams!\n\nRegards,\n\nNUSSU commIT\n\nImportant: This email is confidential and may be privileged. If you are not the intended recipient, please delete it and notify us immediately; you should not copy or use it for any purpose, nor disclose its contents to any other person. Thank you.");
+define("FOOTER_MESSAGE","\nHave fun in recess week!\n\nRegards,\n\nNUSSU commIT\n\nImportant: This email is confidential and may be privileged. If you are not the intended recipient, please delete it and notify us immediately; you should not copy or use it for any purpose, nor disclose its contents to any other person. Thank you.");
 ?>
